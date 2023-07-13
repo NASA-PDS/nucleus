19e86c7ed8c47e10384641f9139248d20fd94ffd
Sagar Joshi <sagar.joshi@jpl.nasa.gov>
2023-07-13 16:20:57 -0700

    diabled terraform workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,6 @@ module "mwaa-env" {
   airflow_execution_role              = var.airflow_execution_role
 }
 
-
 # The following modules are specific to PDS Registry and are under development. These modules are currently
 # capable of successfully deploying some ECS tasks related with PDS Registry. However, these modules
 # are currently disabled to keep the PDS Nucleus Baseline System clean and to avoid confusions.
@@ -18,12 +17,12 @@ module "mwaa-env" {
 #   source = "./terraform-modules/efs"
 # }
 
-module "ecs" {
-  source = "./terraform-modules/ecs"
+# module "ecs" {
+#   source = "./terraform-modules/ecs"
 
-  efs_file_system_id                              = var.efs_file_system_id
-  registry_loader_scripts_access_point_id         = var.registry_loader_scripts_access_point_id
-  registry_loader_default_configs_access_point_id = var.registry_loader_default_configs_access_point_id
-  task_role_arn                                   = var.task_role_arn
-  execution_role_arn                              = var.execution_role_arn
-}
+#   efs_file_system_id                              = var.efs_file_system_id
+#   registry_loader_scripts_access_point_id         = var.registry_loader_scripts_access_point_id
+#   registry_loader_default_configs_access_point_id = var.registry_loader_default_configs_access_point_id
+#   task_role_arn                                   = var.task_role_arn
+#   execution_role_arn                              = var.execution_role_arn
+# }
