b9dc75b592a838d654de79271bd903ec287016ab
Ramesh Maddegoda <94033485+ramesh-maddegoda@users.noreply.github.com>
2024-05-09 01:27:10 -0700

    DELETE Unwanted files

--- terraform/terraform-modules/product-copy-completion-checker/database/execute-sql.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# execute-sql.sh
-
-# Connect to the database and execute SQL file
-mysql --host=terraform-20240418054136603700000001.ckswokhegusf.us-west-2.rds.amazonaws.com --port=3306 --user=admin --password=foobarbaz --database=pds_nucleus  < /Users/rameshm/project/pds/Nucleus/test_prod_prod_deploy_terraform/nucleus/terraform/terraform-modules/product-copy-completion-checker/database/pds-nucleus-database.sql
