4fbaec8d3bb566786f2ed7d3d73e407454e290e1
Ramesh Maddegoda <94033485+ramesh-maddegoda@users.noreply.github.com>
2024-08-06 12:14:02 -0700

    UPDATE variable names for archives

--- terraform/terraform-modules/archive/variables.tf
+++ terraform/terraform-modules/archive/variables.tf
@@ -13,7 +13,7 @@ variable "pds_nucleus_archive_hot_bucket_name_postfix" {
 
 variable "pds_nucleus_archive_cold_bucket_name_postfix" {
   description = "The postfix of the name of the S3 archive cold bucket to receive data to be processed"
-  default     = "archive-hot-<venue-name>"
+  default     = "archive-cold-<venue-name>"
   type        = string
   sensitive   = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -53,6 +53,20 @@ variable "pds_nucleus_staging_bucket_name_postfix" {
   sensitive   = true
 }
 
+variable "pds_nucleus_archive_hot_bucket_name_postfix" {
+  description = "The postfix of the name of the S3 archive hot bucket to receive data to be processed"
+  default     = "archive-hot-<venue-name>"
+  type        = string
+  sensitive   = true
+}
+
+variable "pds_nucleus_archive_cold_bucket_name_postfix" {
+  description = "The postfix of the name of the S3 archive cold bucket to receive data to be processed"
+  default     = "archive-cold-<venue-name>"
+  type        = string
+  sensitive   = true
+}
+
 variable "pds_nucleus_config_bucket_name" {
   description = "PDS Nucleus Configuration S3 Bucket Name"
   default     = "pds-nucleus-config-<venue-name>"
