0ebf6d4fa59468e0a78877a28048baca06784647
Ramesh Maddegoda <94033485+ramesh-maddegoda@users.noreply.github.com>
2024-08-05 19:15:26 -0700

    ADD Placeholder policy files

---
+++ terraform/terraform-modules/ecs-ecr/docker/deploy-ecr-images.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Login to ECR
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com
+
+# Deploy pds-nucleus-config-init ECR image
+cd ./terraform-modules/ecs-ecr/docker/config-init
+docker build -t pds-nucleus-config-init .
+docker tag pds-nucleus-config-init:latest "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-nucleus-config-init:latest
+docker push "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-nucleus-config-init:latest
+
+# Deploy pds-nucleus-s3-to-efs-copy ECR image
+cd ../s3-to-efs-copy
+docker build -t pds-nucleus-s3-to-efs-copy .
+docker tag pds-nucleus-s3-to-efs-copy:latest "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-nucleus-s3-to-efs-copy:latest
+docker push "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-nucleus-s3-to-efs-copy:latest
+
+# Deploy pds-registry-loader-harvest ECR image
+docker image pull nasapds/registry-loader
+docker tag nasapds/registry-loader:latest "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-registry-loader-harvest:latest
+docker push "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-registry-loader-harvest:latest
+
+# Deploy pds-validate ECR image
+docker image pull nasapds/validate
+docker tag nasapds/validate:latest "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-validate:latest
+docker push "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-validate:latest
