8e57115030a8bae22f5f3a3b2713f269690442c0
Ramesh Maddegoda <94033485+ramesh-maddegoda@users.noreply.github.com>
2024-08-08 15:47:05 -0700

    ADD glacier vault

--- terraform/terraform-modules/archive/archive.tf
+++ terraform/terraform-modules/archive/archive.tf
@@ -13,3 +13,9 @@ resource "aws_s3_bucket" "pds_nucleus_col_archive_bucket" {
   # convert PDS node name to S3 bucket name compatible format
   bucket = "${lower(replace(var.pds_node_names[count.index], "_", "-"))}-${var.pds_nucleus_archive_cold_bucket_name_postfix}"
 }
+resource "aws_glacier_vault" "my_arpds_nucleus_cold_archive_glacier_vault" {
+  count = length(var.pds_node_names)
+  name = "${lower(replace(var.pds_node_names[count.index], "_", "-"))}-${var.pds_nucleus_archive_hot_bucket_name_postfix}-archive-glacier-vault"
+}
+
+
