45e828a956d0f6e4c468d713d0391cd30bdffdf3
Ramesh Maddegoda <94033485+ramesh-maddegoda@users.noreply.github.com>
2024-08-12 11:39:34 -0700

    UPDATE archive settings

--- terraform/terraform-modules/archive/archive.tf
+++ terraform/terraform-modules/archive/archive.tf
@@ -1,15 +1,21 @@
 # Terraform script to create the PDS archive related resources
 
-# Create a hot S3 archive bucket for each PDS Node
-resource "aws_s3_bucket" "pds_nucleus_hot_archive_bucket" {
+# Create a hot archive for each PDS Node
+resource "aws_s3_bucket" "pds_nucleus_hot_archive" {
   count = length(var.pds_node_names)
   # convert PDS node name to S3 bucket name compatible format
   bucket = "${lower(replace(var.pds_node_names[count.index], "_", "-"))}-${var.pds_nucleus_hot_archive_name_postfix}"
 }
 
-resource "aws_glacier_vault" "pds_nucleus_cold_archive_glacier_vault" {
+# Create a cold archive for each PDS Node
+resource "aws_glacieraws_s3_bucket_vault" "pds_nucleus_cold_archive" {
   count = length(var.pds_node_names)
+  # convert PDS node name to S3 bucket name compatible format
   name = "${lower(replace(var.pds_node_names[count.index], "_", "-"))}-${var.pds_nucleus_cold_archive_name_postfix}"
+
+  transition {
+    storage_class = GLACIER_IR
+  }
 }
 
 
