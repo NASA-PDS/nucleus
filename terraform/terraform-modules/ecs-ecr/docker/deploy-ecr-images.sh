9e908f456e1241c7f248221d920e8e057537cc21
Ramesh Maddegoda <94033485+ramesh-maddegoda@users.noreply.github.com>
2025-06-11 22:17:06 -0700

    UPDATE to match with template file of deploy-ecr-images.sh

--- terraform/terraform-modules/ecs-ecr/docker/deploy-ecr-images.sh
+++ terraform/terraform-modules/ecs-ecr/docker/deploy-ecr-images.sh
@@ -5,13 +5,13 @@ aws ecr get-login-password --region us-west-2 | docker login --username AWS --pa
 
 # Deploy pds-nucleus-config-init ECR image
 cd ./terraform-modules/ecs-ecr/docker/config-init
-docker build -t pds-nucleus-config-init .
+docker build --platform linux/amd64 -t pds-nucleus-config-init .
 docker tag pds-nucleus-config-init:latest "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-nucleus-config-init:latest
 docker push "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-nucleus-config-init:latest
 
 # Deploy pds-nucleus-s3-to-efs-copy ECR image
 cd ../s3-to-efs-copy
-docker build -t pds-nucleus-s3-to-efs-copy .
+docker build --platform linux/amd64 -t pds-nucleus-s3-to-efs-copy .
 docker tag pds-nucleus-s3-to-efs-copy:latest "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-nucleus-s3-to-efs-copy:latest
 docker push "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-nucleus-s3-to-efs-copy:latest
 
@@ -24,3 +24,8 @@ docker push "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-
 docker image pull nasapds/validate
 docker tag nasapds/validate:latest "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-validate:latest
 docker push "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-validate:latest
+
+# Deploy pds_nucleus_s3_backlog_processor ECR image
+docker image pull nasapds/nucleus-tools-java
+docker tag nasapds/nucleus-tools-java:latest "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-nucleus-tools-java:latest
+docker push "${pds_nucleus_aws_account_id}".dkr.ecr.us-west-2.amazonaws.com/pds-nucleus-tools-java:latest
